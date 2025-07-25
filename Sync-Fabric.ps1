fe3fcfe62e26e6dec001eb933be38c903c6e7ec6
cameron-granger <64664216+cameron-granger@users.noreply.github.com>
2025-07-24 21:25:14 -0500

    Update Sync-Fabric.ps1
    added notice

--- Sync-Fabric.ps1
+++ Sync-Fabric.ps1
@@ -1,3 +1,22 @@
+# Copyright (c) 2025 Hudu Technologies, Inc.
+# All rights reserved.
+#
+# # Redistribution and use of this software in source and binary forms, with or without modification, are permitted under the following conditions:
+#    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions, and the following disclaimer in the 
+#      documentation and/or other materials provided with the distribution
+#    * Neither the name of Hudu Technologies nor the names of its contributors may be used to endorse or promote products derived from this software 
+#      without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS," WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
+# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL HUDU TECHNOLOGIES 
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+#
+# Authors: Mason Stetler
+
+
 param (
     [string]$schemaFile,
     [bool]$dryRun = $false
